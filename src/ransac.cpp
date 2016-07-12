eaa08ed56c77ecde1a0eb4f6f1b918acf426fba0
Pravina Samaratunga <pravinas@mit.edu>
2016-07-12 15:09:52 -0400

    Played around with starting to implement ransac

--- src/ransac.cpp
+++ src/ransac.cpp
@@ -4,27 +4,35 @@
 #include <pcl_conversions/pcl_conversions.h>
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
+#include <pcl/ModelCoefficients.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/sample_consensus/method_types.h>
+#include <pcl/sample_consensus/model_types.h>
+#include <pcl/segmentation/sac_segmentation.h>
 
 ros::Publisher pub;
 
-void 
-cloud_cb (const sensor_msgs::PointCloud2ConstPtr& input)
+sensor_msgs::PointCloud2 removePlane (const sensor_msgs::PointCloud2ConstPtr& input) {
+    // remove plane
+}
+
+void cloud_cb (const sensor_msgs::PointCloud2ConstPtr& input)
 {
   // Create a container for the data.
   sensor_msgs::PointCloud2 output;
 
   // Do data processing here...
+  // Objective: Output should be input without planes
   output = *input;
 
   // Publish the data.
   pub.publish (output);
 }
 
-int
-main (int argc, char** argv)
+int main (int argc, char** argv)
 {
   // Initialize ROS
-  ros::init (argc, argv, "my_pcl_tutorial");
+  ros::init (argc, argv, "ransac_test");
   ros::NodeHandle nh;
 
   // Create a ROS subscriber for the input point cloud
