43105e855b2f61bbfc70783a0b5fa51c9ae58ec3
Pravina Samaratunga <pravinas@mit.edu>
2016-10-03 15:37:05 -0400

    It's been a month since I've done anything and I don't know what's going on here.

--- src/pubsub.cpp
+++ src/pubsub.cpp
@@ -11,6 +11,7 @@
 #include <pcl/sample_consensus/method_types.h>
 #include <pcl/sample_consensus/model_types.h>
 #include <pcl/segmentation/sac_segmentation.h>
+#include <pcl/segmentation/region_growing_rgb.h>
 
 #define COLOR_RED 0xff0000
 #define COLOR_BLUE 0x0000ff
@@ -43,11 +44,10 @@ void filterCloud(pcl::PointCloud<PointT>::Ptr input,
 
 void removeModel(pcl::PointCloud<PointT>::Ptr cloud, 
         pcl::PointCloud<pcl::Normal>::Ptr normals, 
-        int model, 
-        pcl::PointCloud<PointT>::Ptr output) 
+        pcl::SACSegmentationFromNormals<PointT, pcl::Normal> seg,
+        bool negative)
 {
   // Objects needed
-  pcl::SACSegmentationFromNormals<PointT, pcl::Normal> seg; 
   pcl::ExtractIndices<PointT> extract;
   pcl::ExtractIndices<pcl::Normal> extract_normals;
 
@@ -56,12 +56,6 @@ void removeModel(pcl::PointCloud<PointT>::Ptr cloud,
   pcl::PointIndices::Ptr inliers (new pcl::PointIndices);
 
   // Create the segmentation object for the planar model and set all the parameters
-  seg.setOptimizeCoefficients (true);
-  seg.setModelType (model);
-  seg.setNormalDistanceWeight (0.1);
-  seg.setMethodType (pcl::SAC_RANSAC);
-  seg.setMaxIterations (100);
-  seg.setDistanceThreshold (0.05);
   seg.setInputCloud (cloud);
   seg.setInputNormals (normals);
   // Obtain the plane inliers and coefficients
@@ -70,16 +64,11 @@ void removeModel(pcl::PointCloud<PointT>::Ptr cloud,
   // Extract the planar inliers from the input cloud
   extract.setInputCloud (cloud);
   extract.setIndices (inliers);
-  extract.setNegative (false);
-
-  // Write the planar inliers to disk
-  pcl::PointCloud<PointT>::Ptr cloud_plane (new pcl::PointCloud<PointT> ());
-  extract.filter (*output);
 
   // Remove the planar inliers, extract the rest
-  extract.setNegative (true);
+  extract.setNegative (negative);
   extract.filter (*cloud);
-  extract_normals.setNegative (true);
+  extract_normals.setNegative (negative);
   extract_normals.setInputCloud (normals);
   extract_normals.setIndices (inliers);
   extract_normals.filter (*normals);
@@ -89,20 +78,29 @@ void removeModel(pcl::PointCloud<PointT>::Ptr cloud,
 void readerCallback (const sensor_msgs::PointCloud2ConstPtr& input)
 {
   const pcl::PointCloud<PointT>::Ptr cloud(new pcl::PointCloud<PointT>);
-  const pcl::PointCloud<PointT>::Ptr swap(new pcl::PointCloud<PointT>);
-  const pcl::PointCloud<PointT>::Ptr cable1(new pcl::PointCloud<PointT>);
-  const pcl::PointCloud<PointT>::Ptr cable2(new pcl::PointCloud<PointT>);
   const pcl::PointCloud<pcl::Normal>::Ptr normals(new pcl::PointCloud<pcl::Normal>);
 
   pcl::fromROSMsg(*input, *cloud);
   std::cerr << "PointCloud has: " << cloud->points.size () << " data points." << std::endl;
 
-  filterCloud(cloud, cloud, normals);
-  removeModel(cloud, normals, pcl::SACMODEL_NORMAL_PLANE, swap);
-  removeModel(cloud, normals, pcl::SACMODEL_NORMAL_PLANE, swap);
+  pcl::RegionGrowingRGB<PointT> reg_seg;
 
-  //removeModel(cloud, normals, pcl::SACMODEL_CYLINDER, cable1);
-  //removeModel(cloud, normals, pcl::SACMODEL_CYLINDER, cable2);
+  pcl::SACSegmentationFromNormals<PointT, pcl::Normal> sac_seg; 
+  sac_seg.setOptimizeCoefficients (true);
+  sac_seg.setModelType (pcl::SACMODEL_NORMAL_PLANE);
+  sac_seg.setNormalDistanceWeight (0.1);
+  sac_seg.setMethodType (pcl::SAC_RANSAC);
+  sac_seg.setMaxIterations (100);
+  sac_seg.setDistanceThreshold (0.05);
+
+  filterCloud(cloud, cloud, normals);
+  removeModel(cloud, normals, sac_seg, true);
+  sac_seg.setDistanceThreshold (0.035);
+  removeModel(cloud, normals, sac_seg, true);
+
+  //sac_seg.setModelType (pcl::SACMODEL_LINE);
+  //sac_seg.setDistanceThreshold (0.15);
+  //removeModel(cloud, normals, sac_seg, false); 
   
   std::cerr << "PointCloud representing the cylindrical component: " << cloud->points.size () << " data points." << std::endl;
 
@@ -115,7 +113,6 @@ int main(int argc, char** argv)
 {
     ros::init(argc, argv, "pcl_sandbox");
     ros::NodeHandle n;
-    std::cerr << argv[1] << std::endl;
     ros::Subscriber sub = n.subscribe(argv[1], 2, readerCallback); 
     pc_pub = n.advertise<sensor_msgs::PointCloud2>("extracted_cable", 2); 
     ros::spin();
